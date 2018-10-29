3d50604395d98ebbcfc242baafc3fc85bdb27593
tyj <atangyj@gmail.com>
2018-10-29 22:40:52 +0000

    build increment and decrementfunction

--- pomodoro/src/index.js
+++ pomodoro/src/index.js
@@ -7,11 +7,14 @@ const defaultSetting = {
   break_length: 5
 };
 
+
 function SessionSetting(props){
   return(
     <div>
       <div id="session-label">Session Length</div>
       <span id="session-length">{props.session_length}</span>
+      <i className="material-icons" onClick={props.pressIncrement}>add</i>
+      <i className="material-icons" onClick={props.pressDecrement}>remove</i>
     </div>
   )
 }
@@ -28,18 +31,18 @@ function BreakSetting(props){
 function TimerDisplay(props){
   return(
     <div>
-      <div id="time-label"></div>
-      <span id="time-left"></span>
+      <div id="time-label">time-label</div>
+      <span id="time-left">{props.time_left}</span>
     </div>
   )
 }
 
 function PlayControl(props){
-  return <i class="material-icons">{props.isPlaying?"stop":"play_circle_outline"}</i>;
+  return <i className="material-icons">{props.isPlaying?"stop":"play_circle_outline"}</i>;
 }
 
 function ResetControl(props){
-  return <i class="material-icons">replay</i>;
+  return <i className="material-icons">replay</i>;
 }
 
 
@@ -51,20 +54,52 @@ class App extends React.Component {
       break_length: defaultSetting.break_length,
       isPlaying: false,
       isBreak: false,
+      time_left: defaultSetting.session_length * 60,
     };
+    this.renderMinSec = this.renderMinSec.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
+
+  renderMinSec(){
+    let min = Math.floor(this.state.time_left / 60);
+    let second = this.state.time_left % 60;
+    second = Math.floor(second / 10)? second : '0' + second % 10;
+    return `${min}:${second}`;
+  }
+
+  increment(){
+    if(this.state.session_length < 60){
+      this.setState({
+        session_length: this.state.session_length + 1,
+        time_left: this.state.time_left + 60
+      });
+    }
+
+  }
+
+  decrement(){
+    if(this.state.session_length >= 0){
+      this.setState({
+        session_length: this.state.session_length - 1,
+        time_left: this.state.time_left - 60
+      });
+    }
+  }
+
+
 
   render(){
     return(
       <div>
         <h1>Pomodoro Clock</h1>
         <div className="container-settings">
-          <SessionSetting session_length={this.state.session_length}/>
+          <SessionSetting session_length={this.state.session_length} pressIncrement={this.increment} pressDecrement={this.decrement}/>
           <BreakSetting break_length={this.state.break_length}/>
         </div>
 
         <div className="container-display">
-          <TimerDisplay />
+          <TimerDisplay time_left={this.renderMinSec()}/>
         </div>
 
         <div className="container-control">
